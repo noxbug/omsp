6e9f8295460a3aef67f66c93dddfe466dec3293b
noxbug <pieterjan.dingenen@outlook.com>
2020-06-18 00:48:23 +0200

    hwmult

--- main.asm
+++ main.asm
@@ -14,12 +14,24 @@ Disassembly of section .text:
     a006:	b0 12 0a a0 	call	#-24566	;#0xa00a
 
 0000a00a <main>:
-    a00a:	3c 40 20 01 	mov	#288,	r12	;#0x0120
+    a00a:	31 80 06 00 	sub	#6,	r1	;
+    a00e:	b1 42 04 00 	mov	#8,	4(r1)	;r2 As==11
+    a012:	b1 40 05 00 	mov	#5,	2(r1)	;
+    a016:	02 00 
+    a018:	1d 41 02 00 	mov	2(r1),	r13	;
+    a01c:	1c 41 04 00 	mov	4(r1),	r12	;
+    a020:	b0 12 2c a0 	call	#-24532	;#0xa02c
+    a024:	81 4c 00 00 	mov	r12,	0(r1)	;
 
-0000a00e <.Loc.4.1>:
-    a00e:	bc 40 80 5a 	mov	#23168,	0(r12)	;#0x5a80
-    a012:	00 00 
+0000a028 <.L2>:
+    a028:	03 43       	nop			
+    a02a:	fe 3f       	jmp	$-2      	;abs 0xa028
 
-0000a014 <.L2>:
-    a014:	03 43       	nop			
-    a016:	fe 3f       	jmp	$-2      	;abs 0xa014
+0000a02c <__mulhi2>:
+    a02c:	02 12       	push	r2		;
+    a02e:	32 c2       	dint			
+    a030:	03 43       	nop			
+    a032:	82 4c 30 01 	mov	r12,	&0x0130	;
+    a036:	82 4d 38 01 	mov	r13,	&0x0138	;
+    a03a:	1c 42 3a 01 	mov	&0x013a,r12	;0x013a
+    a03e:	00 13       	reti			
--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include "omsp_system.h"
+#include <inttypes.h>
 
 int main(void) {
-  WDTCTL = WDTPW | WDTHOLD;  // Disable watchdog timer
+	//WDTCTL = WDTPW | WDTHOLD;  // Disable watchdog timer
+	
+	int a = 8;
+	int b = 5; 
+	int result = a*b;
+	
+	//result += result;
+	
+	while (1) {
+		__asm__("nop");
+	}
 
-  while (1) {
-	__asm__("nop");
-  }
-
-  return 0;
+	return 0;
 }
